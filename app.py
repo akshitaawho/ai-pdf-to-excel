bf6ef582e691628006de602636b021dd61c87d21
Akshitaa Sahoo <it.akshitaa@herofincorp.biz>
2026-06-16 11:55:52 +0530

    Add PDF upload interface

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
-def home():
-    return {"message": "AI PDF To Excel is running"}
+async def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
+
+
+@app.post("/upload")
+async def upload_pdf(pdf_file: UploadFile = File(...)):
+    return {
+        "filename": pdf_file.filename,
+        "status": "uploaded successfully"
+    }

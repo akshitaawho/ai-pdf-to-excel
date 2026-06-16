b8af26d94f6805ebed8291ce36c4dd17929be6ae
Akshitaa Sahoo <it.akshitaa@herofincorp.biz>
2026-06-16 11:58:11 +0530

    Add PDF text extraction in JSON

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Request, UploadFile, File
 from fastapi.templating import Jinja2Templates
+from services.pdf_parser import extract_text_from_pdf
+import shutil
 
 app = FastAPI()
 
@@ -15,8 +17,18 @@ async def home(request: Request):
 
 
 @app.post("/upload")
-async def upload_pdf(pdf_file: UploadFile = File(...)):
+async def upload_pdf(
+    pdf_file: UploadFile = File(...)
+):
+
+    file_path = f"uploads/{pdf_file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(pdf_file.file, buffer)
+
+    extracted_text = extract_text_from_pdf(file_path)
+
     return {
         "filename": pdf_file.filename,
-        "status": "uploaded successfully"
+        "extracted_text": extracted_text
     }
